2accc6c25b5ddb3bca0e1f5704725ffc3a7b7028
Shashwat Srivastava <shashwat0805@protonmail.com>
2025-08-12 16:26:14 +0530

    getPrimeFactors function added

--- maths.cpp
+++ maths.cpp
@@ -2,6 +2,24 @@
 
 using namespace std;
 
+vector<int> getPrimefactors(int n)
+{
+    vector<int> facts;
+    for (long long d = 2; d * d <= n; d++)
+    {
+        while (n % d == 0)
+        {
+            facts.push_back(d);
+            n /= d;
+        }
+    }
+    if (n > 1)
+    {
+        facts.push_back(n);
+    }
+    return facts;
+}
+
 void getFactors(long long n)
 {
     for (long long i = 1; i * i <= n; i++)
